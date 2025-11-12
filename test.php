c511e29a731f0a61763ceb6f3b18b643a4f8b284
Vasanth <vasanth@webronic.com>
2025-11-12 14:28:19 +0530

    test commit

--- test.php
+++ test.php
@@ -9,10 +9,10 @@ Author: You
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Load the correct version loader
-require_once plugin_dir_path(__FILE__) . 'updater/load-v5p6.php';
+require_once plugin_dir_path(__FILE__) . 'updater/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 // Initialize update checker
-$updateChecker = Puc_v5p6_Factory::buildUpdateChecker(
+$updateChecker = PucFactory::buildUpdateChecker(
     'https://github.com/Vk2401/test-repo/',
     __FILE__,
     'test-repo'
